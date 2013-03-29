600e79e1f27a927cc38f3fc5e9854f7b7f3633b4
Will Hartsell <whartsell@gmail.com>
2013-03-29 13:13:34 -0500

    added mercurial integration

--- src/server/Properties/AssemblyInfo.cs
+++ src/server/Properties/AssemblyInfo.cs
@@ -5,14 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("server")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ALCATEL-LUCENT")]
-[assembly: AssemblyProduct("server")]
-[assembly: AssemblyCopyright("Copyright © ALCATEL-LUCENT 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6b348ce7-763d-418c-9f47-6deff90be1a0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
